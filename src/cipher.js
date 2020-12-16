2c8f50b92d08c184762f2d40a0a00214d13c41de
Mafer's <fersolis518@gmail.com>
2020-12-16 11:22:16 -0600

    add all project to remote repo

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,48 @@
 const cipher = {
   // ...Aqui van tus funciones 
+  //esto es un objeto
   //encode()
   //decode()
+ // fer:function(){
+   // return "Hola Soy FER"
+  //}
+  encode : function (offset, texto){
+    let cifrado='';
+    for (let i = 0; i < texto.length; i++) {
+
+      let ascii = texto.charCodeAt(i);
+      let formula = (ascii - 65 + offset) % 26 + 65;
+      cifrado += String.fromCharCode(formula);
+     
+    }
+    //console.log(cifrado); 
+      return cifrado;
+    },//cifrar
+
+  decode : function (offset, texto){
+    let cifrado='';
+    for (let i = 0; i < texto.length; i++) {
+  
+      let ascii = texto.charCodeAt(i);
+      let formula = (ascii + 65 - offset) % 26 + 65;
+      cifrado += String.fromCharCode(formula);
+        
+      }
+     
+      return cifrado;
+      },//descifrar  
+   
+
+
+  
+  //cipher.encode(offset, string): offset
+  //cipher.decode(offset, string): offset
+
+
 };
+
+//recuerda el proposito
+//mandar a consolas
+//detecta el error
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 import cipher from './cipher.js';
 
+document.getElementById("botonencode").addEventListener("click", function(){
+    let numero=document.getElementById("offset").value;
+    let texto=document.getElementById("textoencode").value;
+    let offset=parseInt(numero);
+    let cifrado=cipher.encode(offset, texto);
+    document.getElementById("resultadoencode").value=cifrado;
+    
+})
+
+document.getElementById("botondecode").addEventListener("click", function(){
+    let numero=document.getElementById("offset").value;
+    let texto=document.getElementById("textodecode").value;
+    let offsetd=parseInt(numero);
+    let descifrar=cipher.decode(offsetd, texto);
+    document.getElementById("resultadodecode").value=descifrar;
+    
+})
+
+document.getElementById("otrocodigo").addEventListener("click", function(){
+    document.getElementById('offset').value = '';
+    document.getElementById("textoencode").value = '';
+    document.getElementById("resultadoencode").value = '';
+    document.getElementById("textodecode").value = '';
+    document.getElementById("resultadodecode").value = '';
+    
+
+})
+      
+
 console.log(cipher);
 console.log("Apurate FER")
+
+
+//console.log(cipher.fer())
+
