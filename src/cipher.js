e79d1451deb348979486db66ae77348db6566d39
Mafer's <fersolis518@gmail.com>
2020-12-18 11:44:34 -0600

    proyect comments

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,38 @@
 const cipher = {
-  // ...Aqui van tus funciones 
+  //Aqui declaro funciones 
   //esto es un objeto
   //Función para cifrar
   encode : function (offset, texto){
-    //if(!offset){ trhrow new typeError ('tu input en invalido')}
-    if (offset === null && texto === null){
-      throw new TypeError ('Bla bla bla');
+    //if(!offset){ trhrow new typeError ('tu input es invalido')}
+    if (offset === null && texto === null){ 
+      throw new TypeError ('Caracter no válido');
     } else
     if (offset === null || texto === []){
-      throw new TypeError ('Bla bla bla');
+      throw new TypeError ('Caracter no válido');
     } else 
     if (offset === 0 || texto === 0){
-      throw new TypeError ('Bla bla bla');
+      throw new TypeError ('Caracter no válido');
     } else {
       let cifrado='';
-      for (let i = 0; i < texto.length; i++) {
+      for (let i = 0; i < texto.length; i++) { //Bucle para hacer algo repetidamente
   
-        let ascii = texto.charCodeAt(i);
-        let formula = (ascii - 65 + offset) % 26 + 65;
-        cifrado += String.fromCharCode(formula);
+        let ascii = texto.charCodeAt(i); //charCodeAt me devuelve un número indicando el valor de mi caracter
+        let formula = (ascii - 65 + offset) % 26 + 65; 
+        cifrado += String.fromCharCode(formula); // fromCharCode me devuelve mi cadena creada 
       } 
-      //Regresa cifrado ok
-        return cifrado;
+        return cifrado; //Regresa cifrado ok
     }
   },
   //Función para descifrar
   decode : function (offset, texto){
     if (offset === null && texto === null){
-      throw new TypeError ('Bla bla bla');
+      throw new TypeError ('Caracter no válido');
     } else
     if (offset === null || texto === []){
-      throw new TypeError ('Bla bla bla');
+      throw new TypeError ('Caracter no válido');
     } else 
     if (offset === 0 || texto === 0){
-      throw new TypeError ('Bla bla bla');
+      throw new TypeError ('Caracter no válido');
     } else { 
       let cifrado='';
       for (let i = 0; i < texto.length; i++) {
@@ -41,8 +40,7 @@ const cipher = {
         let formula = (ascii + 65 - offset) % 26 + 65;
         cifrado += String.fromCharCode(formula);  
         }
-        //Regresa descifrado ok
-        return cifrado;
+        return cifrado; //Regresa descifrado ok
     }
   },  
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+// Pensar en HTML, escucho lo que pasa y hay en mi página, Importo/invoco mis funciones
 import cipher from './cipher.js';
 
-document.getElementById("botonencode").addEventListener("click", function(){
-    let numero=document.getElementById("offset").value;
-    let texto=document.getElementById("textoencode").value;
+document.getElementById("botonencode").addEventListener("click", function(){ //Llamo un evento del DOM
+    let numero=document.getElementById("offset").value; //value me muestra el valor de mi elemento
+    let texto=document.getElementById("textoencode").value; //entro solo al elemento id del html 
     let offset=parseInt(numero);
     let cifrado=cipher.encode(offset, texto);
     document.getElementById("resultadoencode").value=cifrado;
@@ -11,7 +12,7 @@ document.getElementById("botonencode").addEventListener("click", function(){
 
 document.getElementById("botondecode").addEventListener("click", function(){
     let numero=document.getElementById("offset").value;
-    let texto=document.getElementById("textodecode").value;
+    let texto=document.getElementById("textodecode").value; 
     let offsetd=parseInt(numero);
     let descifrar=cipher.decode(offsetd, texto);
     document.getElementById("resultadodecode").value=descifrar;
